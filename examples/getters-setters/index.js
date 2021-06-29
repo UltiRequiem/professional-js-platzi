96503b082e6294034923715c96c7968846d5cf78
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-29 14:12:59 -0500

    :sparkles: Getters y Setters

---
+++ examples/getters-setters/index.js
@@ -0,0 +1,17 @@
+const PERSON = {
+  name: 'Zero',
+  lastname: 'Requiem',
+  get fullName() {
+    return `${this.name} ${this.lastname}`;
+  },
+
+  set fullName(fullName) {
+    const parts = fullName.split(' ');
+    this.name = parts[0] || '';
+    this.lastname = parts[1] || '';
+  },
+};
+
+PERSON.fullName = 'The One';
+
+console.log(PERSON.fullName);
