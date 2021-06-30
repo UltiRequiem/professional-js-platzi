292da934197ba29f51fa34a4208bd6a6f44c122c
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-29 19:14:05 -0500

    :building_construction: Rename some files

---
+++ notes/typescript/index.ts
@@ -0,0 +1,7 @@
+const muted: boolean = true;
+const myNumber: number = 444;
+const myName: string = "Zero Requiem";
+const people: string[] = ["Zero","Oscar","Rockman"];
+// people.push(99) Nope
+
+const peopleAndNumbers: Array< string | number > = ["Zero", 2234]
