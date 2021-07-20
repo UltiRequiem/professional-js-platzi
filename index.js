ed102c8020d738485792549ee736252206417d07
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-19 19:40:15 -0500

    :sparkles: Electron
    npx @electron/check-python-tls

---
+++ index.js
@@ -0,0 +1,14 @@
+// Electron Config
+
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () =>
+  new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+
+
+const run = (window) => window.loadFile('./src/index.html');
+
+app.on('ready', () => run(createWindow()));
